559548fcf132178f8add6de23eced19825dfbba4
Derek Watson <derek.watson92@gmail.com>
2019-04-17 21:41:03 +0200

    changed design slightly, updated logic in JS, added a check box for highest bracket

--- tax-calc.js
+++ tax-calc.js
@@ -1,13 +1,16 @@
-function taxCalculator(income) {
+function taxCalculator(income, start) {
     let taxTable = [[10000000, 0.7], [500000, 0.37], [2000000, 0.35], [157500, 0.32], 
                     [82500, 0.24], [38700, 0.22], [9525, 0.12], [0, 0.10]];
+    if (start === 1) taxTable.shift();
     let tax = bracketCalc(taxTable, taxableIncome(income));
     return tax;
 };
+
 function taxableIncome(income) { 
     if (income > 12000) return income - 12000;
     else return 0;
 };
+
 function bracketCalc(taxTable, income) {
     let tax = 0;
     if (income > 0) {
@@ -20,30 +23,49 @@ function bracketCalc(taxTable, income) {
     }
     return tax;
 };
+
+function displayEmpty() {
+  document.getElementById('income').innerHTML = "";
+  document.getElementById('taxes-paid').innerHTML = "";
+  document.getElementById('takehome').innerHTML = "";
+  document.getElementById('effective').innerHTML = "";
+}
+
 function effective(income, tax) {
     return ((tax / income) * 100).toFixed(2);
 }
-let input = document.querySelector('input');
-input.onkeyup = function() {
-    if (parseFloat(input.value) > 0) {
-        let income = parseFloat(input.value);
-        let tax = taxCalculator(income);
+
+function includeBracket() {
+  if (document.querySelectorAll('.extra-bracket')[0].checked == true) {
+    return 1;
+  } else return 0;
+}
+
+let input = document.querySelectorAll('.income');
+let submission = document.querySelectorAll('.submit-button');
+
+submission[0].onclick = function() {
+    console.log(includeBracket())
+    if (parseFloat(input[0].value) > 0) {
+        let income = parseFloat(input[0].value);
+        let tax = taxCalculator(income, includeBracket());
         let takehome = income - tax;
         let rate = effective(income, tax);
-        let incomeDisp = `Annual income before taxes: ${income.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}`;
-        let taxesDisp = `Tax paid: ${tax.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}`; 
-        let takehomeDisp = `Pay after tax: <strong>${takehome.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</strong>`
-        let effectiveDisp = `Effective tax rate: ${rate}%`
+        let incomeDisp = `<strong>${income.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</strong>`;
+        let taxesDisp = `<strong>${tax.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</strong>`; 
+        let takehomeDisp = `<strong>${takehome.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</strong>`;
+        let effectiveDisp = `<strong>${rate}%</strong>`
         document.getElementById('income').innerHTML = incomeDisp;
         document.getElementById('taxes-paid').innerHTML = taxesDisp;
         document.getElementById('takehome').innerHTML = takehomeDisp;
         document.getElementById('effective').innerHTML = effectiveDisp;
     } else {
-        document.getElementById('income').innerHTML = "";
-        document.getElementById('taxes-paid').innerHTML = "";
-        document.getElementById('takehome').innerHTML = "";
-        document.getElementById('effective').innerHTML = "";
+        displayEmpty();
     }
 };
 
-// (${rate}% Effective Rate)`;
+input[0].onkeyup = function() {
+  if (!parseFloat(input[0].value)) {
+    displayEmpty();
+  } 
+};
